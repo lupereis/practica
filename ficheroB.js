e17103ed159e09b0db1201455851c8c4049b4b7e
lupereis <luisperezeisman@gmail.com>
2023-12-18 13:36:04 +0100

    Actualización del contenido

--- ficheroB.js
+++ ficheroB.js
@@ -1,3 +1,3 @@
-console.log("++++++++++++++++");
-console.log("Soy el fichero B");
-console.log("++++++++++++++++");
+console.log("----------------");
+console.log("Soy el fichero B modificado");
+console.log("----------------");
