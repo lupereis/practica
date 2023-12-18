5145064651cb335b01782b035237d359b3581786
lupereis <luisperezeisman@gmail.com>
2023-12-18 13:41:19 +0100

    Mezcla mirama def

--- ficheroB.js
+++ ficheroB.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-console.log("++++++++++++++++");
-console.log("Soy el fichero 2");
-console.log("++++++++++++++++");
-=======
 console.log("----------------");
 console.log("Soy el fichero B modificado");
 console.log("----------------");
->>>>>>> mirama
